053b1e983f0105f97f4f3d51f9ee3ad9cf7ae259
Ryan Drew <ryan.drew14@gmail.com>
2019-05-24 17:00:08 -0400

    add symlinking for config files, separate out homebrew to macos install script

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
-# install script to setup a mac with my preferred config
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# ================
+#  Install Script
+#  -- general --
+# ================
+
+FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DOT_DIR="$HOME/dotfiles"
+
+if [ ! $DOT_DIR == $FILE_DIR ]; then
+    echo "ERROR: Please move the dotfiles folder to your home directory."
+    exit 1
+fi
+
+# symlink config files into correct directories
+ln -s ~/dotfiles/bash/.bash_profile ~/.bash_profile
+mkdir -p .config/nvim && ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
---
+++ install_macos.sh
@@ -0,0 +1,10 @@
+# ================
+#  Install Script
+#   -- macos --
+# ================
+
+# call general install script
+./install.sh
+
+# install homebrew (macOS package manager)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
